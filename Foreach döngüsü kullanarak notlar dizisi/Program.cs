0feba24b3d84faa921d3cbe33d22669f122960a0
Serap BALCI <81075168+reset-34@users.noreply.github.com>
2024-08-01 16:09:03 +0300

    Add project files.

---
+++ Foreach döngüsü kullanarak notlar dizisi/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Foreach_döngüsü_kullanarak_notlar_dizisi
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] notlar = { 100, 50, 85, 90, 25, 45, 37, 75, 87 };
+
+            foreach(int not in notlar)
+            {
+                if (not >= 50) 
+                {
+                    Console.WriteLine(not + "Notun İyi ");
+                }
+                else
+                {
+                    Console.WriteLine(not + "Notun Kötü");
+                }
+                
+            }
+            Console.ReadLine();
+        }
+    }
+}
